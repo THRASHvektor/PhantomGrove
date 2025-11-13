0307bd61e62bae2aa06bc1e9ad63100dbfcac666
Cai Shiteng <1132357884@qq.com>
2025-11-13 16:56:03 +0800

    Change open system menu UI bingding to left controller X 1.add input action "open menu" 2.add input binding to bind controller left X to action open menu

--- Assets/SteamVR_Input/SteamVR_Input_Actions.cs
+++ Assets/SteamVR_Input/SteamVR_Input_Actions.cs
@@ -43,6 +43,8 @@ namespace Valve.VR
         
         private static SteamVR_Action_Vector2 p_default_turn;
         
+        private static SteamVR_Action_Boolean p_default_openMenu;
+        
         private static SteamVR_Action_Vibration p_default_Haptic;
         
         private static SteamVR_Action_Vector2 p_platformer_Move;
@@ -165,6 +167,14 @@ namespace Valve.VR
             }
         }
         
+        public static SteamVR_Action_Boolean default_openMenu
+        {
+            get
+            {
+                return SteamVR_Actions.p_default_openMenu.GetCopy<SteamVR_Action_Boolean>();
+            }
+        }
+        
         public static SteamVR_Action_Vibration default_Haptic
         {
             get
@@ -253,6 +263,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_SnapTurnRight,
                     SteamVR_Actions.default_TouchPad,
                     SteamVR_Actions.default_turn,
+                    SteamVR_Actions.default_openMenu,
                     SteamVR_Actions.default_Haptic,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
@@ -276,6 +287,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_SnapTurnRight,
                     SteamVR_Actions.default_TouchPad,
                     SteamVR_Actions.default_turn,
+                    SteamVR_Actions.default_openMenu,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -299,6 +311,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_HeadsetOnHead,
                     SteamVR_Actions.default_SnapTurnLeft,
                     SteamVR_Actions.default_SnapTurnRight,
+                    SteamVR_Actions.default_openMenu,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Brake,
                     SteamVR_Actions.buggy_Reset};
@@ -326,6 +339,7 @@ namespace Valve.VR
                     SteamVR_Actions.default_SnapTurnRight,
                     SteamVR_Actions.default_TouchPad,
                     SteamVR_Actions.default_turn,
+                    SteamVR_Actions.default_openMenu,
                     SteamVR_Actions.platformer_Move,
                     SteamVR_Actions.platformer_Jump,
                     SteamVR_Actions.buggy_Steering,
@@ -350,6 +364,7 @@ namespace Valve.VR
             SteamVR_Actions.p_default_SnapTurnRight = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/SnapTurnRight")));
             SteamVR_Actions.p_default_TouchPad = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/default/in/TouchPad")));
             SteamVR_Actions.p_default_turn = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/default/in/turn")));
+            SteamVR_Actions.p_default_openMenu = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/default/in/openMenu")));
             SteamVR_Actions.p_default_Haptic = ((SteamVR_Action_Vibration)(SteamVR_Action.Create<SteamVR_Action_Vibration>("/actions/default/out/Haptic")));
             SteamVR_Actions.p_platformer_Move = ((SteamVR_Action_Vector2)(SteamVR_Action.Create<SteamVR_Action_Vector2>("/actions/platformer/in/Move")));
             SteamVR_Actions.p_platformer_Jump = ((SteamVR_Action_Boolean)(SteamVR_Action.Create<SteamVR_Action_Boolean>("/actions/platformer/in/Jump")));
